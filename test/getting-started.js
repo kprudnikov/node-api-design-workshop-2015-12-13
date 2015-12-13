a0423d3599802ca3c61bd9f435756f25cc27bc76
Konstantin <kprudnikov@gmail.com>
2015-12-13 11:25:16 +0200

    first test

---
+++ test/getting-started.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const chai = require('chai'),
+  assert = chai.assert;
+
+const fetch = require('node-fetch');
+const server = require('../src/hello-server');
+
+describe('hello world', () => {
+  before(done => server.listen(4000, done));
+  after(() => server.close());
+
+  it('should respond to requests', done => {
+    fetch('http://localhost:4000').then(response => {
+      assert(response.ok, 'Hello world response');
+      done();
+    })
+  });
+});
