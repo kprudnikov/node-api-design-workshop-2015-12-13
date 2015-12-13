c765ee7ea5141154c588d1d7ea9d41743cb62649
Konstantin <kprudnikov@gmail.com>
2015-12-13 16:38:41 +0200

    auth, files refactor, code coverage

--- src/jsonapi-server.js
+++ src/jsonapi-server.js
@@ -3,18 +3,18 @@
 const jsonApi = require('jsonapi-server');
 
 jsonApi.listen = (port, callback) => {
-  jsonApi.setConfig({port, base: 'rest'});
+  jsonApi.setConfig({ port });
 
   jsonApi.start();
   if (callback) {
     process.nextTick(callback);
   }
-}
+};
 
 const Joi = jsonApi.Joi;
 
 jsonApi.define({
-  resource: "articles",
+  resource: 'articles',
   handlers: new jsonApi.MemoryHandler(),
   attributes: {
     title: Joi.string().required(),
@@ -23,20 +23,16 @@ jsonApi.define({
   },
 
   examples: [{
-    // id is always a string (for memory handler)
-    id: '11',
-    // type always multiple
-    type: 'articles',
+    id: '11', // should be a string for memory handler
+    type: 'articles', // should be same as "resource"
     title: 'Hello World',
     body: 'Exciting'
-  },
-  {
-    id: '12',
-    type: 'articles',
-    title: 'Fresh news',
-    body: 'Astonishing'
-  }
-  ]
+  }, {
+    id: '12', // should be a string for memory handler
+    type: 'articles', // should be same as "resource"
+    title: 'Fresh News',
+    body: 'Intresting'
+  }]
 });
 
 module.exports = jsonApi;
--- test/rest-api.js
+++ test/rest-api.js
@@ -1,22 +1,52 @@
 'use strict';
 
-const chai = require('chai');
-const assert = chai.assert;
+const chai = require('chai'),
+  assert = chai.assert;
 
-const fetch = require('node-fetch');
-const co = require('co');
-const server = require('../src/jsonapi-server');
+const fetch = require('node-fetch'),
+  co = require('co');
+
+const jsonapiServer = require('../src/jsonapi-server'),
+  authServer = require('../src/auth-proxy');
 
 describe('REST API', () => {
-  before(done => server.listen(5000, done));
-  after(() => server.close());
+  before(done => jsonapiServer.listen(5000, done));
+  after(() => jsonapiServer.close());
 
-  it('should respond to requests',
-    co.wrap(function* (){
-      const response = yield fetch('http://localhost:5000/rest/articles');
-      assert(response.ok, '/articles response');
-      const json = yield response.json();
-      console.log(json);
-      assert(json.data[0].id === '11', 'Should have data');
+  it('/articles', co.wrap(function* () {
+    const response = yield fetch('http://localhost:5000/articles');
+    assert(response.ok, '/articles response');
+    const json = yield response.json();
+    assert(json.data[0].id === '11', 'should have data');
+  }));
+
+  describe('Authentication & Authorization', () => {
+    before(done => authServer.listen(6000, done));
+    after(() => authServer.close());
+
+    it('should authorize', co.wrap(function* () {
+      const authResponse = yield fetch('http://localhost:6000/oauth/token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          // real-life OAuth2 requests are conducted over HTTPS only
+          'Authorization': 'Basic ' + new Buffer('thom:nightworld').toString('base64')
+        },
+        body: 'grant_type=password&username=thomseddon&password=nightworld'
+      });
+      assert(authResponse.ok, '/login response');
+      const authJson = yield authResponse.json();
+      assert(authJson.access_token, 'No token was sent');
+
+      const articleResponse = yield fetch('http://localhost:6000/articles/11', {
+        headers: {
+          'Authorization': 'Bearer ' + authJson.access_token
+        }
+      });
+      assert(articleResponse.ok, '/articles/11 response');
+      const articleJson = yield articleResponse.json();
+      assert(articleJson.data.id === '11', 'should have data');
     }));
+  });
+
 });
