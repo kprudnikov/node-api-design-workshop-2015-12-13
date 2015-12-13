a83b065eb82569650fecc671a8c135a361bf17f3
Konstantin <kprudnikov@gmail.com>
2015-12-13 11:42:13 +0200

    switched to generators for async code

--- test/getting-started.js
+++ test/getting-started.js
@@ -1,19 +1,21 @@
 'use strict';
 
-const chai = require('chai'),
-  assert = chai.assert;
+const chai = require('chai');
+const assert = chai.assert;
 
 const fetch = require('node-fetch');
+const co = require('co');
 const server = require('../src/hello-server');
 
 describe('hello world', () => {
   before(done => server.listen(4000, done));
   after(() => server.close());
 
-  it('should respond to requests', done => {
-    fetch('http://localhost:4000').then(response => {
+  it('should respond to requests',
+    co.wrap(function* (){
+      const response = yield fetch('http://localhost:4000');
       assert(response.ok, 'Hello world response');
-      done();
-    })
-  });
+      const text = yield response.text();
+      assert(text === 'Hello World', 'Should say Hello World');
+    }));
 });
